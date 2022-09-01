021ae1e314cf49ed0a287979c5d35f983ec0c4a1
haidan168 <1531637406@qq.com>
2022-09-01 22:24:49 +0800

    select by ul

--- 06.CSS下拉选择/select-by-ul/index.js
+++ 06.CSS下拉选择/select-by-ul/index.js
@@ -1,1 +1,16 @@
-console.log("!!!!");
+const selectDiv = document.getElementsByClassName("select")[0];
+document.getElementsByClassName("select")[0].addEventListener('click',(e) => {
+  const option = document.getElementsByClassName("select-options")[0];
+  if(e.target.nodeName === 'DIV'){   
+    if(option.style.display === 'inline'){
+      option.style.display = 'none';
+    }else{
+      option.style.display = 'inline';
+    }
+  }
+  if(e.target.nodeName === 'LI'){
+    const p = document.getElementsByClassName('selected-val')[0];
+    p.innerText = e.target.innerText;
+    option.style.display = 'none';
+  }
+});
